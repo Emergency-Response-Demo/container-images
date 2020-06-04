ed6d713c988f74950fdfc8a31c28f192715b74d7
Bernard Tison <bernard.tison@gmail.com>
2020-06-04 16:56:22 +0200

    ubi8-java8-mvn image

---
+++ ubi8-java8-mvn/README.md
@@ -0,0 +1,7 @@
+### ubi8-java8-mvn
+
+Image derived from `quay.io/btison/ub8-java8`
+
+Adds runtime maven support as required by RHPAM applications.
+
+Runtime image for Emergency Response Demo Process Service
---
+++ ubi8-java8-mvn/contrib/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+if [ -n "${MAVEN_MIRROR_URL}" ]; then
+
+  echo "Set Maven mirror to ${MAVEN_MIRROR_URL}"
+
+  xml="<mirror>\n\
+      <id>mirror.default</id>\n\
+      <url>${MAVEN_MIRROR_URL}</url>\n\
+      <mirrorOf>external:*</mirrorOf>\n\
+    </mirror>\n\
+    <!-- ### configured mirrors ### -->"
+
+  sed -i "s|<!-- ### configured mirrors ### -->|$xml|" "/m2/settings.xml"
+
+fi
+
+bash /deployments/run-java.sh 
